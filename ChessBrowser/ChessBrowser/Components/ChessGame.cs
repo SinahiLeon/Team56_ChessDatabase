5db05ff04d5bfa763ba971e2f080534c06d3aec3
SINAHI LEON <Sinahi.Leon1@gmail.com>
2026-03-01 11:23:11 -0700

    Made very simple getter/setters for ChessGame properties. Made static class for PGN files, parse method will return a list of the contents of the pgn file

---
+++ ChessBrowser/ChessBrowser/Components/ChessGame.cs
@@ -0,0 +1,56 @@
+﻿namespace ChessBrowser.Components
+{
+    public class ChessGame
+    {
+        public string Event
+        {
+            get; set;
+        }
+
+        public string Site
+        {
+            get; set;
+        }
+
+        public string Round
+        {
+            // "not always convertible to a number since sometimes there are two decimal points, such as 1.4.10"
+            get; set;
+        }
+
+        public string White
+        {
+            get; set;
+        }
+
+        public string Black
+        {
+            get; set;
+        }
+
+        public char Result
+        {
+            get; set;
+        }
+
+        public int WhiteElo
+        {
+            get; set;
+        }
+
+        public int BlackElo
+        {
+            get; set;
+        }
+
+        public string EventDate
+        {
+            get; set;
+        }
+
+        public string Moves
+        {
+            get; set;
+        }
+    }
+}
---
+++ ChessBrowser/ChessBrowser/Components/PgnParser.cs
@@ -0,0 +1,15 @@
+﻿namespace ChessBrowser.Components
+{
+    public static class PgnParser
+    {
+        public static List<ChessGame> Parse(string pgnText)
+        {
+            List<ChessGame> games = new List<ChessGame>();
+
+
+            return games;
+        }
+
+
+    }
+}
